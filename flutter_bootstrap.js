ec813a177c161dcd04bf8dbcb460968093de9b1a
= <=>
2025-06-05 11:21:16 -0300

    Publicar build web no gh-pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3312620796"
+    serviceWorkerVersion: "1372934231"
   }
 });
